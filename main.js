2be21c2af766ab1250858d08efe202d812b84817
AshleyFlores-G <142762578+AshleyFlores-G@users.noreply.github.com>
2024-06-25 00:56:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-//add hovered class to selected list item
-let list = document.querySelectorAll(.navigation li);
+// Add hovered class to selected list item
+let list = document.querySelectorAll(".navigation li");
 
-function activelink(){
-    list.forEach(item=>{
-        item.classlist.remove("hovered");
+function activelink() {
+    list.forEach(item => {
+        item.classList.remove("hovered");
     });
     this.classList.add("hovered");
 }
 
-list.forEach(item => item.addEventListener("mouseover, activelink"));
+list.forEach(item => item.addEventListener("mouseover", activelink));
+
+// TOGGLE MENU
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function() {
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+};
+
